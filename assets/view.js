a42840c7b6cefad4ea4aa7ec672b380c16f80289
Your Name <you@example.com>
2025-09-25 20:01:07 +0300

    patch challenge template (CTFd frontend breaking changes)

--- assets/view.js
+++ assets/view.js
@@ -1,25 +1,21 @@
 CTFd._internal.challenge.data = undefined
 
-CTFd._internal.challenge.renderer = CTFd.lib.markdown();
+CTFd._internal.challenge.renderer = null;
 
 CTFd._internal.challenge.preRender = function () {
 }
 
-CTFd._internal.challenge.render = function (markdown) {
-    return CTFd._internal.challenge.renderer.render(markdown)
-}
+CTFd._internal.challenge.render = null;
 
 CTFd._internal.challenge.postRender = function () {
     loadInfo();
 }
 
-if ($ === undefined) $ = CTFd.lib.$;
+if (window.$ === undefined) window.$ = CTFd.lib.$;
 
 function loadInfo() {
-    var challenge_id = $('#challenge-id').val();
+    var challenge_id = CTFd._internal.challenge.data.id;
     var url = "/api/v1/plugins/ctfd-whale/container?challenge_id=" + challenge_id;
-
-    var params = {};
 
     CTFd.fetch(url, {
         method: 'GET',
@@ -44,68 +40,40 @@ function loadInfo() {
             window.t = undefined;
         }
         if (response.success) response = response.data;
-        else CTFd.ui.ezq.ezAlert({
+        else CTFd._functions.events.eventAlert({
             title: "Fail",
-            body: response.message,
+            html: response.message,
             button: "OK"
         });
-        if (response.remaining_time === undefined) {
-            $('#whale-panel').html('<div class="card" style="width: 100%;">' +
-                '<div class="card-body">' +
-                '<h5 class="card-title">Instance Info</h5>' +
-                '<button type="button" class="btn btn-primary card-link" id="whale-button-boot" ' +
-                '        onclick="CTFd._internal.challenge.boot()">' +
-                'Launch an instance' +
-                '</button>' +
-                '</div>' +
-                '</div>');
-        } else {
-            $('#whale-panel').html(
-                `<div class="card" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title">Instance Info</h5>
-                        <h6 class="card-subtitle mb-2 text-muted" id="whale-challenge-count-down">
-                            Remaining Time: ${response.remaining_time}s
-                        </h6>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            Lan Domain: ${response.lan_domain}
-                        </h6>
-                        <p id="user-access" class="card-text"></p>
-                        <button type="button" class="btn btn-danger card-link" id="whale-button-destroy"
-                                onclick="CTFd._internal.challenge.destroy()">
-                            Destroy this instance
-                        </button>
-                        <button type="button" class="btn btn-success card-link" id="whale-button-renew"
-                                onclick="CTFd._internal.challenge.renew()">
-                            Renew this instance
-                        </button>
-                    </div>
-                </div>`
-            );
-            $('#user-access').html(response.user_access);
+        if (response.remaining_time != undefined) {
+            $('#whale-challenge-user-access').html(response.user_access);
+            $('#whale-challenge-lan-domain').html(response.lan_domain);
+            $('#whale-challenge-count-down').text(response.remaining_time);
+            $('#whale-panel-stopped').hide();
+            $('#whale-panel-started').show();
 
-            function showAuto() {
-                const c = $('#whale-challenge-count-down')[0];
-                if (c === undefined) return;
-                const origin = c.innerHTML;
-                const second = parseInt(origin.split(": ")[1].split('s')[0]) - 1;
-                c.innerHTML = 'Remaining Time: ' + second + 's';
-                if (second < 0) {
+            window.t = setInterval(() => {
+                const c = $('#whale-challenge-count-down').text();
+                if (!c) return;
+                let second = parseInt(c) - 1;
+                if (second <= 0) {
                     loadInfo();
                 }
-            }
-
-            window.t = setInterval(showAuto, 1000);
+                $('#whale-challenge-count-down').text(second);
+            }, 1000);
+        } else {
+            $('#whale-panel-started').hide();
+            $('#whale-panel-stopped').show();
         }
     });
 };
 
 CTFd._internal.challenge.destroy = function () {
-    var challenge_id = $('#challenge-id').val();
+    var challenge_id = CTFd._internal.challenge.data.id;
     var url = "/api/v1/plugins/ctfd-whale/container?challenge_id=" + challenge_id;
 
-    $('#whale-button-destroy')[0].innerHTML = "Waiting...";
-    $('#whale-button-destroy')[0].disabled = true;
+    $('#whale-button-destroy').text("Waiting...");
+    $('#whale-button-destroy').prop('disabled', true);
 
     var params = {};
 
@@ -130,29 +98,30 @@ CTFd._internal.challenge.destroy = function () {
     }).then(function (response) {
         if (response.success) {
             loadInfo();
-            CTFd.ui.ezq.ezAlert({
+            CTFd._functions.events.eventAlert({
                 title: "Success",
-                body: "Your instance has been destroyed!",
+                html: "Your instance has been destroyed!",
                 button: "OK"
             });
         } else {
-            $('#whale-button-destroy')[0].innerHTML = "Destroy this instance";
-            $('#whale-button-destroy')[0].disabled = false;
-            CTFd.ui.ezq.ezAlert({
+            CTFd._functions.events.eventAlert({
                 title: "Fail",
-                body: response.message,
+                html: response.message,
                 button: "OK"
             });
         }
+    }).finally(() => {
+        $('#whale-button-destroy').text("Destroy this instance");
+        $('#whale-button-destroy').prop('disabled', false);
     });
 };
 
 CTFd._internal.challenge.renew = function () {
-    var challenge_id = $('#challenge-id').val();
+    var challenge_id = CTFd._internal.challenge.data.id;
     var url = "/api/v1/plugins/ctfd-whale/container?challenge_id=" + challenge_id;
 
-    $('#whale-button-renew')[0].innerHTML = "Waiting...";
-    $('#whale-button-renew')[0].disabled = true;
+    $('#whale-button-renew').text("Waiting...");
+    $('#whale-button-renew').prop('disabled', true);
 
     var params = {};
 
@@ -177,29 +146,30 @@ CTFd._internal.challenge.renew = function () {
     }).then(function (response) {
         if (response.success) {
             loadInfo();
-            CTFd.ui.ezq.ezAlert({
+            CTFd._functions.events.eventAlert({
                 title: "Success",
-                body: "Your instance has been renewed!",
+                html: "Your instance has been renewed!",
                 button: "OK"
             });
         } else {
-            $('#whale-button-renew')[0].innerHTML = "Renew this instance";
-            $('#whale-button-renew')[0].disabled = false;
-            CTFd.ui.ezq.ezAlert({
+            CTFd._functions.events.eventAlert({
                 title: "Fail",
-                body: response.message,
+                html: response.message,
                 button: "OK"
             });
         }
+    }).finally(() => {
+        $('#whale-button-renew').text("Renew this instance");
+        $('#whale-button-renew').prop('disabled', false);
     });
 };
 
 CTFd._internal.challenge.boot = function () {
-    var challenge_id = $('#challenge-id').val();
+    var challenge_id = CTFd._internal.challenge.data.id;
     var url = "/api/v1/plugins/ctfd-whale/container?challenge_id=" + challenge_id;
 
-    $('#whale-button-boot')[0].innerHTML = "Waiting...";
-    $('#whale-button-boot')[0].disabled = true;
+    $('#whale-button-boot').text("Waiting...");
+    $('#whale-button-boot').prop('disabled', true);
 
     var params = {};
 
@@ -224,26 +194,27 @@ CTFd._internal.challenge.boot = function () {
     }).then(function (response) {
         if (response.success) {
             loadInfo();
-            CTFd.ui.ezq.ezAlert({
+            CTFd._functions.events.eventAlert({
                 title: "Success",
-                body: "Your instance has been deployed!",
+                html: "Your instance has been deployed!",
                 button: "OK"
             });
         } else {
-            $('#whale-button-boot')[0].innerHTML = "Launch an instance";
-            $('#whale-button-boot')[0].disabled = false;
-            CTFd.ui.ezq.ezAlert({
+            CTFd._functions.events.eventAlert({
                 title: "Fail",
-                body: response.message,
+                html: response.message,
                 button: "OK"
             });
         }
+    }).finally(() => {
+        $('#whale-button-boot').text("Launch an instance");
+        $('#whale-button-boot').prop('disabled', false);
     });
 };
 
 
 CTFd._internal.challenge.submit = function (preview) {
-    var challenge_id = $('#challenge-id').val();
+    var challenge_id = CTFd._internal.challenge.data.id;
     var submission = $('#challenge-input').val()
 
     var body = {
